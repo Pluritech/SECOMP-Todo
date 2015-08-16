6db52a70efba0a45f74571b5e7c851f3f5bfbeb5
pluritech <luishmcmoreno@gmail.com>
2015-08-16 07:13:25 -0300

    Salvando no localstorage a cada mudança na lista de tarefas

--- www/js/services.js
+++ www/js/services.js
@@ -3,6 +3,20 @@ angular.module('SecompTodo')
 	.factory('Tarefas', function () {
 
 		var tarefas = [];
+		var TAREFAS_KEY = 'TAREFAS_KEY';
+
+		function salvarLista() {
+			localStorage.setItem(TAREFAS_KEY, JSON.stringify(tarefas));
+		};
+
+		var listaSalva = localStorage.getItem(TAREFAS_KEY);
+		if (listaSalva) {
+			try {
+				tarefas = JSON.parse(listaSalva);
+			} catch (erro) {
+				tarefas = [];
+			}
+		}
 
 		return {
 			getTarefas: function () {
@@ -11,10 +25,12 @@ angular.module('SecompTodo')
 
 			criarTarefa: function (tarefa) {
 				tarefas.push(tarefa);
+				salvarLista();
 			},
 
 			concluirTarefa: function (tarefa) {
 				tarefa.concluida = true;
+				salvarLista();
 			}
 		};
 	});
